f8d2b05030fc90b46b3d27e1d1c3aff0637c07f8
BeniHR <hreniucbenii@gmail.com>
2022-11-09 23:10:16 +0200

    Making the navbar/ starting the home pace section

---
+++ HomePage/home.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+navbarLinks.classList.toggle('active');
+console.log(toggleButton)
+})
+});
